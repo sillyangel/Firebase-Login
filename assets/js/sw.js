0e684a9c0ff48ce70ef49ebc7c358785e97c5820
SIllyangel3 <50461810+banana-nana@users.noreply.github.com>
2021-12-10 21:04:11 +0000

    Add files via upload

---
+++ assets/js/sw.js
@@ -0,0 +1,53 @@
+var cacheName = '';
+var cacheAssets = [
+	'/Pages/Home2.html',
+	'/Pages/Normal.css',
+	'/Pages/Game.js',
+	'/Pages/',
+	'appmanifest.json',
+
+];
+
+// Call install Event
+self.addEventListener('install', e => {
+	// Wait until promise is finished
+	e.waitUntil(
+		caches.open(cacheName)
+		.then(cache => {
+			console.log(`Service Worker: Caching Files: ${cache}`);
+			cache.addAll(cacheAssets)
+				// When everything is set
+				.then(() => self.skipWaiting())
+		})
+	);
+})
+
+
+
+
+
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+	console.log('Service Worker: Fetching');
+	e.respondWith(
+		fetch(e.request)
+		.then(res => {
+			// The response is a stream and in order the browser
+			// to consume the response and in the same time the
+			// cache consuming the response it needs to be
+			// cloned in order to have two streams.
+			const resClone = res.clone();
+			// Open cache
+			caches.open(cacheName)
+				.then(cache => {
+					// Add response to cache
+					cache.put(e.request, resClone);
+				});
+			return res;
+		}).catch(
+			err => caches.match(e.request)
+			.then(res => res)
+		)
+	);
+});
